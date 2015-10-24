31325bf523477bf3061932396f1d07fefa17c923
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-10-24 17:58:12 +1100

    Fix error thrown by destroy.
    When no arguments were passed an error was raised.

--- src/mapper/destruction.js
+++ src/mapper/destruction.js
@@ -1,3 +1,4 @@
+import { isEmpty } from 'lodash/lang';
 import { NotFoundError, NoRowsFoundError } from '../errors';
 
 const methods = {
@@ -48,7 +49,7 @@ const methods = {
    *   Promise resolving to the number of rows deleted.
    */
   destroy(...ids) {
-    const mapper = this.target(...ids);
+    const mapper = isEmpty(ids) ? this : this.target(...ids);
     const queryBuilder = mapper.toDestroyQuery();
     return queryBuilder.then(response =>
       this._handleDestroyResponse({ response, queryBuilder })
