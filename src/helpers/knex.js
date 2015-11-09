7eea57b024cd064306c3bfdc38a7e8aa48f1a938
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-11-09 22:47:12 +1100

    Remove unused file

--- src/helpers/knex.js
+++
@@ -1,8 +0,0 @@
-/**
- * Workaround for Knex not having an `unlimit` method.
- */
-export function unlimitQuery(query) {
-  const copy = query.clone();
-  delete copy._single.limit;
-  return copy;
-}
