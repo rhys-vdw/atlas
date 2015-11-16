821fe25c2bdb822c7b0e449652d8273721e35f7c
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-11-17 00:22:46 +1100

    Add test for `withRelated`.

--- tests/integration/mapper/relations.js
+++ tests/integration/mapper/relations.js
@@ -42,42 +42,39 @@ export default function(atlas) {
         ])
       ).then(() => {
 
-        st.resolvesToDeep(
-          Users.related('avatar', 1).fetch(),
-          { id: 10, user_id: 1, image_path: './dean.jpg' },
-          `Mapper#related(relation, id) resolves correctly`
-        );
-
-        st.resolvesToDeep(
-          Users.related('avatar', { id: 2, name: 'Sarah' }).fetch(),
-          { id: 12, user_id: 2, image_path: './sarah.jpg' },
-          `Mapper#related(relation, {id}) resolves correctly`
-        );
-
-        st.resolvesToDeep(
-          Users.related('avatar', 1, 3).fetch(), [
+        return Promise.join(
+          st.resolvesToDeep(
+            Users.related('avatar', 1).fetch(),
             { id: 10, user_id: 1, image_path: './dean.jpg' },
-            { id: 11, user_id: 3, image_path: './bazza.jpg' },
-          ], `Mapper#related(relation, id, id) resolves correctly`
-        );
-
-        st.resolvesToDeep(
-          Users.related('avatar', [1, 3]).fetch(), [
-            { id: 10, user_id: 1, image_path: './dean.jpg' },
-            { id: 11, user_id: 3, image_path: './bazza.jpg' },
-          ], `Mapper#related(relation, [id, id]) resolves correctly`
-        );
-
-        st.resolvesTo(
-          Users.related('avatar', { id: 4 }).fetch(),
-          null,
-          `Mapper#related(relation, {id}) resolves to null if none found`
-        );
-
-        st.resolvesToDeep(
-          Users.related('avatar', [{ id: 4 }, { id: 6 }]).fetch(),
-          [],
-          `Mapper#related(relation, [{id}, {id}]) resolves to [] if none found`
+            `Mapper#related(relation, id) resolves correctly`
+          ),
+          st.resolvesToDeep(
+            Users.related('avatar', { id: 2, name: 'Sarah' }).fetch(),
+            { id: 12, user_id: 2, image_path: './sarah.jpg' },
+            `Mapper#related(relation, {id}) resolves correctly`
+          ),
+          st.resolvesToDeep(
+            Users.related('avatar', 1, 3).fetch(), [
+              { id: 10, user_id: 1, image_path: './dean.jpg' },
+              { id: 11, user_id: 3, image_path: './bazza.jpg' },
+            ], `Mapper#related(relation, id, id) resolves correctly`
+          ),
+          st.resolvesToDeep(
+            Users.related('avatar', [1, 3]).fetch(), [
+              { id: 10, user_id: 1, image_path: './dean.jpg' },
+              { id: 11, user_id: 3, image_path: './bazza.jpg' },
+            ], `Mapper#related(relation, [id, id]) resolves correctly`
+          ),
+          st.resolvesTo(
+            Users.related('avatar', { id: 4 }).fetch(),
+            null,
+            `Mapper#related(relation, {id}) resolves to null if none found`
+          ),
+          st.resolvesToDeep(
+            Users.related('avatar', [{ id: 4 }, { id: 6 }]).fetch(),
+            [],
+            `Mapper#related(relation, [{id}, {id}]) resolves to [] if none found`
+          )
         );
       });
     });
@@ -101,34 +98,72 @@ export default function(atlas) {
         const baz = { id: 3, name: 'Baz' };
         const other = { id: 4, name: 'Other' };
 
-        st.resolvesToDeep(
-          Users.loadInto('avatar', dean),
-          { id: 1, name: 'dean', avatar: {
-              id: 10, user_id: 1, image_path: './dean.jpg'
-          } },
-          'loads into single record'
+        return Promise.join(
+          st.resolvesToDeep(
+            Users.loadInto('avatar', dean),
+            { id: 1, name: 'dean', avatar: {
+                id: 10, user_id: 1, image_path: './dean.jpg'
+            } },
+            'loads into single record'
+          ),
+          st.resolvesToDeep(
+            Users.loadInto('avatar', [sarah, baz]), [
+              { id: 2, name: 'Sarah', avatar:
+                { id: 12, user_id: 2, image_path: './sarah.jpg' }
+              }, { id: 3, name: 'Baz', avatar:
+                { id: 11, user_id: 3, image_path: './bazza.jpg' }
+              }
+            ], 'loads into multiple records'
+          ),
+          st.resolvesToDeep(
+            Users.loadInto('avatar', [dean, other]), [
+              { id: 1, name: 'dean', avatar:
+                { id: 10, user_id: 1, image_path: './dean.jpg' }
+              }, { id: 4, name: 'Other', avatar: null }
+            ], 'loads null into one record and null into another'
+          )
         );
-
-
-        st.resolvesToDeep(
-          Users.loadInto('avatar', [sarah, baz]), [
-            { id: 2, name: 'Sarah', avatar:
-              { id: 12, user_id: 2, image_path: './sarah.jpg' }
-            }, { id: 3, name: 'Baz', avatar:
-              { id: 11, user_id: 3, image_path: './bazza.jpg' }
-            }
-          ], 'loads into multiple records'
-        );
-
-        st.resolvesToDeep(
-          Users.loadInto('avatar', [dean, other]), [
-            { id: 1, name: 'dean', avatar:
-              { id: 10, user_id: 1, image_path: './dean.jpg' }
-            }, { id: 4, name: 'Other', avatar: null }
-          ], 'loads null into one record and null into another'
-        );
-
       });
     });
+
+    t.databaseTest('`Mapper#withRelated()`', knex, {
+      users: usersTable, avatars: avatarsTable
+    }, st => {
+
+      const Avatars = Mapper.table('avatars');
+
+      const Users = Mapper.table('users').relations({
+        avatar: hasOne(Avatars)
+      });
+
+      return knex('avatars').insert([
+        { id: 10, user_id: 1, image_path: './dean.jpg' },
+        { id: 11, user_id: 3, image_path: './bazza.jpg' },
+        { id: 12, user_id: 2, image_path: './sarah.jpg' },
+      ]).then(() => knex('users').insert([
+        { id: 1, name: 'Dean' },
+        { id: 2, name: 'Sarah' },
+        { id: 3, name: 'Baz' },
+        { id: 4, name: 'Other guy' }
+      ])).then(() => {
+
+        return st.resolvesToDeep(
+          Users.withRelated('avatar').fetch(), [
+            { id: 1, name: 'Dean', avatar:
+              { id: 10, user_id: 1, image_path: './dean.jpg' }
+            },
+            { id: 2, name: 'Sarah', avatar:
+              { id: 12, user_id: 2, image_path: './sarah.jpg' }
+            },
+            { id: 3, name: 'Baz', avatar:
+              { id: 11, user_id: 3, image_path: './bazza.jpg' }
+            },
+            { id: 4, name: 'Other guy', avatar: null }
+          ], 'fetches records with related models'
+        );
+      });
+
+    });
+
   });
 }
