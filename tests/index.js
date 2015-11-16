036c71cd9d389745d04ea7544bde9c50396d08aa
Rhys van der Waerden <rhys.vdw@gmail.com>
2015-11-16 23:21:02 +1100

    Remove junk

--- tests/index.js
+++ tests/index.js
@@ -1,18 +1,5 @@
-//import 'tape-catch';
+import 'tape-catch';
 import './custom-tests';
 
 import './unit';
 import './integration';
-
-/*
-import test from 'tape';
-
-test.onFinish(() => {
-  console.log("DONE!");
-  process.exit();
-});
-
-test.createStream()
-  //.on('data', d => console.log('thing: ', d))
-  .on('end', d => console.log('END:', d));
-  */
