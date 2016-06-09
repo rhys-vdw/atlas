37a9153e20b6a4be592141f4c3798317c583f8d9
Rhys van der Waerden <rhys.vdw@gmail.com>
2016-06-10 03:00:39 +1000

    Update mapper docs

--- src/mapper/mapper.js
+++ src/mapper/mapper.js
@@ -5,36 +5,85 @@ import Chain from '../Chain';
  * @extends Chain
  * @classdesc
  *
- * Mappers represent a set of data in your database. A mapper can be scoped or
- * specialized by chaining calls to its methods.
+ * Chainable interface to a subset of your database.
  *
- * Mappers are immutable, so any setter method will return a copy of the Mapper
- * insance with the new settings. `Mapper` instances need never be instantiated
- * with `new`, instead each method call that would mutate the instance returns a
- * copy.
+ * Mappers are immutable, so any method that would modify its state will return
+ * a copy instead.
  *
  * ```js
- * // Get the base mapper from `atlas`.
- * const Mapper = atlas('Mapper');
+ * import Mapper from 'atlas/Mapper';
  *
- * // Create a new Mapper that represents users.
- * const Users = Mapper.table('users').idAttribute('user_id');
+ * // Create an instance.
+ * const mapper = new Mapper();
  *
- * // Create one from `Users` that represents administrators.
- * const Admins = Users.where('is_admin', true);
+ * // Create a new mapper for the users table.
+ * const users = mapper.table('users').idAttribute('user_id');
+ *
+ * // Now target a subset of rows.
+ * const admins = users.where('is_admin', true);
  *
  * // select * from users where is_admin = true;
- * Admins.fetch().then(admins => {
+ * atlas(admins).fetch().then(admins => {
  *   // ...
  * });
  * ```
  *
- * These docs instead use the convention of naming mappers in `PascalCase` and
- * records in `camelCase`. This is okay because the `Mapper` constructor never
- * appears in your code.
+ * You can use ES6 inheritance to extend the interface to your needs.
  *
  * ```js
- * Cars.fetch().then(cars => // ...
+ * class SoftDeleteMapper extends Mapper {
+ *
+ *   initialize() {
+ *     this.defaultAttributes({
+ *       is_deleted: false
+ *     })
+ *   }
+ *
+ *   // Override
+ *   destroy(...records) {
+ *     return this.target(...records).updateAll({ is_deleted: true })
+ *   }
+ *
+ *   // Override
+ *   destroyAll() {
+ *     return this.updateAll({ is_deleted: true })
+ *   }
+ *
+ *   current() {
+ *     return this.where({ is_deleted: false })
+ *   }
+ *
+ *   deleted() {
+ *     return this.where({ is_deleted: true })
+ *   }
+ * }
+ * ```
+ *
+ * Or you can use the {@link extend} instance method to include a mixin.
+ *
+ * ```js
+ * const messages = mapper.table('messages').extend({
+ *
+ *   unread() {
+ *     return this.where('seen', false)
+ *   },
+ *
+ *   read() {
+ *     return this.where('seen', true);
+ *   }
+*
+ * });
+ * ```
+ *
+ * ```js
+ * import { TimeStamp, CamelCase } from 'atlas/plugins';
+ *
+ * const base = mapper.extend(TimeStamp, CamelCase);
+ *
+ * const users = base.table('users');
+ * const accounts = base.table('accounts');
+ *
+ * atlas.register(users, accounts);
  * ```
  *
  */
