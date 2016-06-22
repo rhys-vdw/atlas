e77f7a3f6d58a068d94a02c1e8b859a5e41e9755
Rhys van der Waerden <rhys.vdw@gmail.com>
2016-06-22 21:15:13 +1000

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const tape = require('gulp-tape');
+const tapColorize = require('tap-colorize');
+const eslint = require('gulp-eslint');
+const exec = require('child_process').exec;
+
+gulp.task('build', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('lib'));
+});
+
+gulp.task('build-tests', () => {
+  return gulp.src('tests/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('compiled-tests'));
+});
+
+gulp.task('default', () => {
+  gulp.watch(['src/**/*.js', 'tests/**/*.js'], ['build-and-test']);
+});
+
+function runTests(callback) {
+  exec(
+    'node compiled-tests/index.js | $(npm bin)/tap-colorize',
+    (err, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      callback(err);
+    }
+  );
+}
+
+gulp.task('build-and-test', ['build', 'build-tests'], runTests);
+gulp.task('test', runTests);
